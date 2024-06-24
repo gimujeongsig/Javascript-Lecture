85410d7cbed3f9d76a95ae02ca8510519cd82cf7
gimujeongsig <dbehcuwjc@gmail.com>
2024-06-24 15:25:43 +0900

    스페이스 인베이다

--- 스페이스 인베이다/main.js
+++ 스페이스 인베이다/main.js
@@ -5,19 +5,23 @@ for(let i=0; i < stageSize*stageSize; i++) {
     const box = document.createElement("div");
     document.querySelector(".grid").appendChild(box);
 }
-
 const grid = document.querySelector(".grid");
 const stage = document.querySelectorAll(".grid div");
 const startBtn = document.querySelector("#startBtn");
-const stopBtn = document.querySelector("stopBtn");
-const display = document.querySelector(".grid div");
+const stopBtn = document.querySelector("#stopBtn");
+const display = document.querySelector("#display");
 
-let playerLoc = stageSize*(stageSize-2)+7 
+let playerLoc = stageSize*(stageSize-2)+7;
+let invadersLoc = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+];
+let gameInterval;
+
 
 function makePlayer() {
     stage[playerLoc].classList.add("player");
 }
-makePlayer()
 
 function movePlayer(e) {
     stage[playerLoc].classList.remove("player");
@@ -32,21 +36,60 @@ function movePlayer(e) {
     stage[playerLoc].classList.add("player");
 }
 
-document.addEventListener("keyup", movePlayer)
 
 let invaderLoc = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39
 ]
-let gameInterval;
 
 function makeInvader() {
     invaderLoc.forEach(function(invader) {
         stage[invader].classList.add("invader");
     })
 }
-makeInvader();
-function moveInvader() {  }
 
-interval = setInterval(moveInvader, 1000);6
+function moveInvader() {
+    invaderLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader");
+    })
+    for(let i=0; i < invaderLoc.length; i++) {
+        invaderLoc[i]++;
+        stage[invaderLoc[i]].classList.add("invader");
+    }
+}
+
+function gameStart() {
+    stage[playerLoc].classList.remove("player");
+    invaderLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader");
+    });
+    playerLoc = stageSize*(stageSize-2)+7
+    invaderLoc = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    ]
+    makePlayer();
+    makeInvader();
+    displayStatus();
+    gameInterval = setInterval(moveInvader, 1000);
+    document.addEventListener("keyup", movePlayer);
+    gameRun();
+}
+
+function gameStop() {
+    clearInterval(gameInterval);
+    document.removeEventListener("keyup", movePlayer);
+}
+ 
+function gameRun() {
+    moveInvader();
+}
+
+function displayStatus() {
+    display.innerText = invaderLoc.length + "/" + invaderLoc.length
+}
+
+startBtn.addEventListener("click", gameStart);
+stopBtn.addEventListener("click", gameStop);
